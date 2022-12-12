6800a6dc3845760125f4489ce0a478d42024f046
HASSANALIGOHAR <sp19bsse0016@maju.edu.pk>
2022-12-12 19:39:31 +0500

    devops

---
+++ DevOpsautomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevOpsautomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevOpsautomation")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d43017f2-12b7-499b-a94e-f07e988f2db6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DevOpsautomation/UnitTest1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Security.Policy;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+
+
+namespace DevOpsautomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Login()
+        {
+            WebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.saucedemo.com/";
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+            Thread.Sleep(1000);
+            driver.Close();            
+        } 
+
+        [TestMethod]
+        public void AddProduct()
+        {
+            WebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.saucedemo.com/";
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack")).Click();
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-bike-light")).Click();
+            Thread.Sleep(1000);
+            //driver.FindElement(By.Id("remove-sauce-labs-backpack")).Click();  //remove item
+            //scroll down
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollBy(0,300)");
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-bolt-t-shirt")).Click();
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-fleece-jacket")).Click();
+            js.ExecuteScript("window.scrollBy(0,200)");
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-onesie")).Click();
+            driver.FindElement(By.Id("add-to-cart-test.allthethings()-t-shirt-(red)")).Click();
+            Thread.Sleep(500);
+            js.ExecuteScript("window.scrollBy(0,100)");
+            Thread.Sleep(1000);
+            driver.Close();
+        }
+    
+    [TestMethod]
+    public void ClickOnSoicalIcon()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
+        driver.Url = "https://www.saucedemo.com/";
+        Thread.Sleep(1000);
+        driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+        driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+        driver.FindElement(By.Id("login-button")).Click();
+        Thread.Sleep(1000);        
+        //driver.FindElement(By.Id("remove-sauce-labs-backpack")).Click();
+        //scroll down
+        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+        js.ExecuteScript("window.scrollBy(0,800)");
+        Thread.Sleep(1000);
+            driver.FindElement(By.ClassName("social_twitter")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.ClassName("social_facebook")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.ClassName("social_linkedin")).Click();
+            Thread.Sleep(1000);
+        }
+    }
+
+}
